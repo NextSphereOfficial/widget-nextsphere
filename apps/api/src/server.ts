7fd40f6c708626f459cc26293a090a88db8b65d3
NextSphere <nextsphere.official@gmail.com>
2025-10-26 17:16:18 +0100

    connectivity test2

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -16,6 +16,21 @@ Sentry.init({
   tracesSampleRate: 0.0,
   sendDefaultPii: true,
 });
+
+// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+// If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
+require("./instrument.js");
+
+// All other imports below
+const { createServer } = require("node:http");
+
+const server = createServer((req, res) => {
+  // server code
+});
+
+server.listen(3000, "127.0.0.1");
+
+
 // subito dopo Sentry.init(...)
 app.get('/sentry-error-test', async () => {
   try {
